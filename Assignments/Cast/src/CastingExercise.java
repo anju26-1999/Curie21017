b0f885327db6d37c528f8e0ab31e4c856a39a054
anju26-1999 <84728647+anju26-1999@users.noreply.github.com>
2021-07-07 11:37:11 +0530

    Add files via upload

---
+++ Assignments/Cast/src/CastingExercise.java
@@ -0,0 +1,18 @@
+
+public class CastingExercise {
+
+	public static void main(String[] args) {
+		byte b=10;
+		int i=4;
+		byte c='i';
+		byte d=(byte)i;
+		byte x=10;
+		byte y=20;
+		byte sum=(byte)(x*y);
+		System.out.println("byte value...:"+d);
+		System.out.println("int value...:"+i);
+		
+
+	}
+
+}
